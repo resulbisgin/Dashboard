d9eeb0dfb0f794249707176a66db9c0885d56f9c
resulbisgin <resulbiginresul_20011@hotmail.com>
2023-01-24 23:56:54 +0300

    add sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
-
+import './app.css'
 function App() {
   return (
     <>
     <Topbar/>
-   
+      <div className="container">
+    <Sidebar/>
+      <div className="others">others</div>
+      </div>
     </>
   );
 }
