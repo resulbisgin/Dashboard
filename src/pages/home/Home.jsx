7e0b0db86e16ec4d2c22bf726325b446461d16a6
resulbisgin <resulbiginresul_20011@hotmail.com>
2023-01-25 04:00:21 +0300

    add chart

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,12 @@
 import "./home.css"
 import  Featuredinfo from "../../components/featuredInfo/Featuredinfo"
+import Chart from "../../components/chart/Chart"
+import { userData } from "../../dummuyData"
 export default function Home() {
   return (
     <div className='home'>
         <Featuredinfo/>
+        <Chart data={userData} title='User Analytics' grid dataKey="Active User"/>
     </div>
   )
 }
