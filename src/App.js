14643c326bceef34e81d750ff41907165d6dde35
resulbisgin <resulbiginresul_20011@hotmail.com>
2023-01-31 01:33:35 +0300

    productList

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import UserList from "./pages/userList/UserList";
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList";
 function App() {
   return (
     <>
@@ -18,6 +19,9 @@ function App() {
         <Route path='/user' element={<UserList/>}/>
         <Route path='/user/:userId' element={<User/>}/>
         <Route path='/newUser' element={<NewUser/>}/>
+        <Route path='/products' element={<ProductList/>}/>
+        <Route path='/products/:productsId' element={<User/>}/>
+        <Route path='/newproduct' element={<NewUser/>}/>
     </Routes>
     </BrowserRouter>
       </div>
